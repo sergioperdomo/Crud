3cc3d3efc7dddd79dc41e3e90f187a8cc25371ac
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-16 21:16:04 -0500

    Creando los diferentes métodos, pero se esta trabajando especificamente en el método insertar.

---
+++ CrudJava/src/main/java/org/example/Estudiantes.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+public class Estudiantes extends  JFrame{
+    private JPanel panel;
+    private JTextField telText;
+    private JTextField apellidoText;
+    private JTextField edadText;
+    private JTextField direccionText;
+    private JButton ingresarButton;
+    private JButton consultarButton;
+    private JList lista;
+    private JTextField idText;
+    private JTextField nombreText;
+
+    Connection connection;
+    PreparedStatement ps;
+
+    public Estudiantes() {
+        consultarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conectar();
+            }
+        });
+    }
+
+    public void listar(){
+        conectar();
+    }
+
+    public void insertar() throws SQLException {
+        conectar();
+        ps = connection.prepareStatement("INSERT INTO estudiante VALUES (?,?,?,?,?,?)");
+        ps.setInt(1, Integer.parseInt(idText.getText()));
+        ps.setString(2, nombreText.getText());
+        ps.setString(3, apellidoText.getText());
+        ps.setInt(4, Integer.parseInt(edadText.getText()));
+        ps.setString(5, telText.getText());
+        ps.setString(6, direccionText.getText());
+
+    }
+
+
+
+    public static void main (String[] args){
+        Estudiantes f =  new Estudiantes();
+        f.setContentPane(new Estudiantes().panel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.pack();
+    }
+
+
+    public void conectar (){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/learning", "root", "");
+            System.out.println("Conectado!!!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
